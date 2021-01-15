8ffd00543ceb1c6f2b10ce493b88f808fab27a53
Frederik Hounsvad <frederik@Ghounsvad.dk>
2021-01-15 19:50:58 +0100

    Random likely non functional changes

--- Server/backend/obj/Debug/net5.0/backend.AssemblyInfo.cs
+++ Server/backend/obj/Debug/net5.0/backend.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("0392dd1e-e7ac-4cf4-b403-c73b1f05d4e4")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
