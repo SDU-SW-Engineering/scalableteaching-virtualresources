b51e8203f04aacf76ed895ca33085afa5562ca3c
Frederik Hounsvad <frederik@hounsvad.dk>
2021-03-16 20:11:55 +0100

    UpdateHostnameLinux

--- Server/Programs_to_change_files_on_vm/UpdateHostName/main.cpp
+++ Server/Programs_to_change_files_on_vm/UpdateHostName/main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <fstream>
-#include <array>
+#include <string>
+#include <vector>
 #define HOSTS_FILE "/etc/hosts"
 #define HOSTNAME_FILE "/etc/hostname"
 
-void println(std::string output);
+void println(const std::string& output);
 bool validateHostname(char *hostname);
+
 
 int main(int argc, char **argv) {
     //Validate that input has been given
@@ -15,14 +17,60 @@ int main(int argc, char **argv) {
     }
     if(!validateHostname(argv[1])) return 1;//validateHostname prints its own debug messages
 
-    //TODO: Change hostname in hostname file
+    std::string oldHostName;
+    std::string newHostName = argv[1];
+    {
+        //Read Current Contents
+        std::ifstream inputFileStream(HOSTNAME_FILE, std::ios::in);
+        std::getline(inputFileStream, oldHostName);
+        inputFileStream.close();
 
-    //TODO: Change hostname in hosts file (Look at the name stored in hostname file and search in this one)
+        //Write new name
+        std::fstream outputFileStream(HOSTNAME_FILE, std::ios::out | std::ios::trunc);
+        outputFileStream.write(newHostName.c_str(), newHostName.length());
+        outputFileStream.flush();
+        if(outputFileStream.bad()){
+            println("Error on write /etc/hostname");
+            outputFileStream.close();
+            return 1;
+        }
+        outputFileStream.close();
+    }
 
-    return 0;
+    {
+        std::vector<std::string> lineVector = std::vector<std::string>(); //Create a new empty vector
+        std::string line; // Pre declare the line for scanning lines
+        std::ifstream inputFileStream(HOSTS_FILE, std::ios::in);
+        while (std::getline(inputFileStream, line)){
+            int indexOfHostName = static_cast<int>(line.find(oldHostName));
+            if(indexOfHostName < line.length()){ //Found hostname in line
+                std::string newLine = std::string("");
+                newLine.append(line.substr(0, indexOfHostName));
+                newLine.append(newHostName);
+                newLine.append("\n");
+                lineVector.push_back(newLine);
+            }else{
+                line.append("\n");
+                lineVector.push_back(line);
+            }
+        }
+        inputFileStream.close();
+        std::ofstream outputFileStream(HOSTS_FILE, std::ios::out | std::ios::trunc);
+        for(const auto& l : lineVector){
+            outputFileStream.write(l.c_str(),l.length());
+        }
+        outputFileStream.flush();
+        if(outputFileStream.bad()){
+            println("Error on write /etc/hosts");
+            outputFileStream.close();
+            return 1;
+        }else{
+            return 0;
+        }
+    }
 }
 
-void println(std::string output){
+void println(const std::string& output){
     std::cout << output << std::endl;
 }
 
@@ -48,4 +96,5 @@ bool validateHostname(char *hostname){
             return false;
         }
     }
+    return true;
 }
