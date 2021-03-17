039d278b427b336507fc3fe724c60798af4f9db4
Frederik Hounsvad <frederik@hounsvad.dk>
2021-03-17 20:45:21 +0100

    Added boostlib
    Added boostlib dependency
    Reformatted updateHostname to be a method

--- Server/Programs_to_change_files_on_vm/UpdateHostName/main.cpp
+++ Server/Programs_to_change_files_on_vm/UpdateHostName/main.cpp
@@ -2,14 +2,49 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <boost/json.hpp>
 #define HOSTS_FILE "/etc/hosts"
 #define HOSTNAME_FILE "/etc/hostname"
 
 void println(const std::string& output);
 bool validateHostname(char *hostname);
-
+int updateHostname(int& argc, char **argv);
 
 int main(int argc, char **argv) {
+    updateHostname(argc, argv);
+
+}
+
+void println(const std::string& output){
+    std::cout << output << std::endl;
+}
+
+bool validateHostname(char *hostname){
+    //Validate Hostname
+    char *newHostName = hostname;
+    int newHostNameSize = (sizeof(newHostName)/sizeof(newHostName[0]));
+    if(newHostNameSize<1 || newHostNameSize > 63){
+        println("The hostname cannot be smaller than 1 character or greater than 63 characters");
+        return false;
+    }
+    if(newHostName[0] == '-'){
+        println("Hostname Cannot start with a '-'");
+        return false;
+    }
+
+    for(int i = 0 ; i < newHostNameSize; i++){
+        if(!( //If char is not one of the following then:
+                (newHostName[i] >= 97 && newHostName[i] <= 122) ||  //char is a-z
+                newHostName[i] == '-' ||                            //char is -
+                (newHostName[i] >= 48 && newHostName[i] <= 57 ))){    //char is 0-9
+            println("Hostname can only contain the characters a-z and '-' and cannot start with '-'");
+            return false;
+        }
+    }
+    return true;
+}
+
+int updateHostname(int& argc, char **argv){
     //Validate that input has been given
     if(argc < 2){
         println("The program takes the new hostname as first argument");
@@ -70,31 +105,6 @@ int main(int argc, char **argv) {
     }
 }
 
-void println(const std::string& output){
-    std::cout << output << std::endl;
-}
+int addSystemUser(){
 
-bool validateHostname(char *hostname){
-    //Validate Hostname
-    char *newHostName = hostname;
-    int newHostNameSize = (sizeof(newHostName)/sizeof(newHostName[0]));
-    if(newHostNameSize<1 || newHostNameSize > 63){
-        println("The hostname cannot be smaller than 1 character or greater than 63 characters");
-        return false;
-    }
-    if(newHostName[0] == '-'){
-        println("Hostname Cannot start with a '-'");
-        return false;
-    }
-
-    for(int i = 0 ; i < newHostNameSize; i++){
-        if(!( //If char is not one of the following then:
-                (newHostName[i] >= 97 && newHostName[i] <= 122) ||  //char is a-z
-                newHostName[i] == '-' ||                            //char is -
-                (newHostName[i] >= 48 && newHostName[i] <= 57 ))){    //char is 0-9
-            println("Hostname can only contain the characters a-z and '-' and cannot start with '-'");
-            return false;
-        }
-    }
-    return true;
 }
