91c6e518bb8a2fe34fced1105055fcf7cf533320
Frederik Hounsvad <frederik@hounsvad.dk>
2021-07-18 08:04:10 +0200

    Updated namespaces and implemented a put to update all members of a group

---
+++ Server/backend/DTO/AllGroupMembersDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ScalableTeaching.DTO
+{
+    public class AllGroupMembersDTO
+    {
+        public Guid GroupID { get; set; }
+        public List<string> Usernames { get; set; }
+
+        public bool Validate()
+        {
+            if (GroupID != Guid.Empty || Usernames != null || Usernames.Count == 0 ) return false;
+            foreach(string user in Usernames)
+            {
+                if (user.Length == 0 || user.Length > 7) return false;
+            }
+            return true;
+        }
+    }
+}
--- Server/backend/Helpers/MachineCreationProcessor.cs
+++ Server/backend/Helpers/MachineCreationProcessor.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Backend.Helpers
+namespace ScalableTeaching.Helpers
 {
     public class MachineCreationProcessor
     {
