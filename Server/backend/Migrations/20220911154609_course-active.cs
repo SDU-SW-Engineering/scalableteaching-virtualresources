a9a6e9310fef2f351573428f917ce060a8ff1531
Frederik Hounsvad <frederik@hounsvad.dk>
2022-09-11 18:52:53 +0200

    Migration

---
+++ Server/backend/Migrations/20220911154609_course-active.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ScalableTeaching.Migrations
+{
+    public partial class courseactive : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "MachineMemmoryUtilizationBytes",
+                table: "MachineStatuses",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "MachineLCMState",
+                table: "MachineStatuses",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "MachineCpuUtilizationPercent",
+                table: "MachineStatuses",
+                type: "numeric",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "numeric");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Active",
+                table: "Courses",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Active",
+                table: "Courses");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "MachineMemmoryUtilizationBytes",
+                table: "MachineStatuses",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "MachineLCMState",
+                table: "MachineStatuses",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "MachineCpuUtilizationPercent",
+                table: "MachineStatuses",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "numeric",
+                oldNullable: true);
+        }
+    }
+}
